2993564594c14d0c8d965d0cf3bdc1c6db556a15
Ben Staton <statonbe@gmail.com>
2021-01-11 11:41:23 -0800

    Add a script that houses packages

---
+++ 00-packages.R
@@ -0,0 +1,16 @@
+# ::::::::::::::::::::::::::::::::::::::::::::::::::: #
+# SCRIPT TO HOUSE REQUIRED PACKAGES                   #
+# ::::::::::::::::::::::::::::::::::::::::::::::::::: #
+
+# SOURCE THIS SCRIPT AT THE TOP OF KEY SCRIPTS THAT WILL NEED ANY PACKAGE
+# ALL PACKAGES AVAILABLE ON CRAN
+# USE `install.packages("pkgName")` to install them
+
+suppressPackageStartupMessages({
+  library(glmmTMB)  # for efficient fitting of complex GLMs
+  library(DHARMa)   # for standardized residual diagnostics
+  library(stringr)  # for misc. string manipulations
+  library(lme4)     # for bootMer() - provides nice interface for parametric bootstrap
+  library(boot)     # for boot.ci() - summarizes output of lme4::bootMer()
+  library(scales)   # for transparent colors
+})
