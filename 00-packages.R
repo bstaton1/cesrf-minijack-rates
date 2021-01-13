7f0e393f201f9d37af264bb9599e0a8c1d24e56f
Ben Staton <statonbe@gmail.com>
2021-01-13 14:55:44 -0800

    Also suppress warnings when loading packages
    Will prevent things like: Warning, package XXXXX was built under version X.X.X. These are fairly pointless and may just confuse readers

--- 00-packages.R
+++ 00-packages.R
@@ -6,13 +6,15 @@
 # ALL PACKAGES AVAILABLE ON CRAN
 # USE `install.packages("pkgName")` to install them
 
-suppressPackageStartupMessages({
-  library(glmmTMB)  # for efficient fitting of complex GLMs
-  library(DHARMa)   # for standardized residual diagnostics
-  library(stringr)  # for misc. string manipulations
-  library(lme4)     # for bootMer() - provides nice interface for parametric bootstrap
-  library(boot)     # for boot.ci() - summarizes output of lme4::bootMer()
-  library(scales)   # for transparent colors
-  library(snow)     # for parallel computing - makes parametric bootstrap much faster
-  library(rlecuyer) # for random number generation in parallel computing
+suppressWarnings({
+  suppressPackageStartupMessages({
+    library(glmmTMB)  # for efficient fitting of complex GLMs
+    library(DHARMa)   # for standardized residual diagnostics
+    library(stringr)  # for misc. string manipulations
+    library(lme4)     # for bootMer() - provides nice interface for parametric bootstrap
+    library(boot)     # for boot.ci() - summarizes output of lme4::bootMer()
+    library(scales)   # for transparent colors
+    library(snow)     # for parallel computing - makes parametric bootstrap much faster
+    library(rlecuyer) # for random number generation in parallel computing
+  })
 })
