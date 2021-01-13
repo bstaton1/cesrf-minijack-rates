82ebaf052683de030193e1c6240f9a7139802590
Ben Staton <statonbe@gmail.com>
2021-01-13 15:03:54 -0800

    Add code for figure showing minijack rate vs. progeny weight

--- 04-ms-output.R
+++ 04-ms-output.R
@@ -45,3 +45,92 @@ ests = ests[,c("year", "type", "id", "estimate", "lwr95", "upr95", "p_val")]
 # save to a csv file
 write.csv(ests, file.path(out_dir, "bootstrap-summaries.csv"), row.names = F)
 
+##### CREATE FIGURE: FIXED-EFFECT MINIJACK RATE BY PROGENY WEIGHT, UNCERTAINTY, AND DATA #####
+
+# refit the null models
+fit_null_14 = glmmTMB(minijack ~ progeny_wt + (1|sire_id) + (1|dam_id),
+                      data = subset(dat, year == 2014), family = binomial)
+fit_null_15 = glmmTMB(minijack ~ progeny_wt + (1|sire_id) + (1|dam_id),
+                      data = subset(dat, year == 2015), family = binomial)
+fit_null_16 = glmmTMB(minijack ~ progeny_wt + (1|sire_id) + (1|dam_id),
+                      data = subset(dat, year == 2016), family = binomial)
+
+# calculate summary statistics for each weight category (for plotting data only)
+agg1_14 = aggregate(minijack ~ wt_cat, data = subset(dat, year == 2014), mean); colnames(agg1_14) = c("wt_cat", "p_minijack")
+agg2_14 = aggregate(minijack ~ wt_cat,data = subset(dat, year == 2014), length); colnames(agg2_14) = c("wt_cat", "n_smolt")
+agg_14 = merge(agg1_14, agg2_14, by = "wt_cat"); agg_14 = cbind(year = 2014, agg_14)
+
+agg1_15 = aggregate(minijack ~ wt_cat, data = subset(dat, year == 2015), mean); colnames(agg1_15) = c("wt_cat", "p_minijack")
+agg2_15 = aggregate(minijack ~ wt_cat,data = subset(dat, year == 2015), length); colnames(agg2_15) = c("wt_cat", "n_smolt")
+agg_15 = merge(agg1_15, agg2_15, by = "wt_cat"); agg_15 = cbind(year = 2015, agg_15)
+
+agg1_16 = aggregate(minijack ~ wt_cat, data = subset(dat, year == 2016), mean); colnames(agg1_16) = c("wt_cat", "p_minijack")
+agg2_16 = aggregate(minijack ~ wt_cat,data = subset(dat, year == 2016), length); colnames(agg2_16) = c("wt_cat", "n_smolt")
+agg_16 = merge(agg1_16, agg2_16, by = "wt_cat"); agg_16 = cbind(year = 2016, agg_16)
+
+agg = rbind(agg_14, agg_15, agg_16)
+
+# calculate the midpoint of each weight category
+x = stringr::str_split(agg$wt_cat, ",")
+x = lapply(x, function(y) stringr::str_remove_all(y, "\\]"))
+x = lapply(x, function(y) stringr::str_remove_all(y, "\\("))
+agg$mp = unlist(lapply(x, function(y) sum(as.numeric(y))/2))
+
+# set point shapes and sizes based on sample size
+agg$pch = ifelse(agg$n_smolt < 30, 22, 21)
+agg$cex = ifelse(agg$n_smolt < 30, 1.25, agg$n_smolt * 0.01)
+
+png(file.path(out_dir, "fixed-effect-progeny-wt.png"), h = 6 * ppi, w = 2.75 * ppi, res = ppi)
+par(mar = c(1,1,1,1), oma = c(2,3,0,0), mfrow = c(3,1), mgp = c(2,0.35,0), tcl = -0.15, cex.axis = 1.2)
+
+# 2014 plot
+plot(p_minijack ~ mp, data = subset(agg, year == 2014), las = 1, type = "n", xlim = range(dat$progeny_wt) + c(-5,5), ylim = c(0,1))
+probs = get_probs(fit_null_14, "contrast", F)
+probs = probs[probs$sire_age == 3,]
+polygon(x = c(probs$progeny_wt, rev(probs$progeny_wt)),
+        y = c(probs$lwr95ci, rev(probs$upr95ci)), col = scales::alpha("grey25", 0.25), border = NA)
+lines(lwr95ci ~ progeny_wt, data = probs, col = "grey")
+lines(upr95ci ~ progeny_wt, data = probs, col = "grey")
+lines(prob ~ progeny_wt, data = probs, col = "black", lwd = 2)
+box()
+points(p_minijack ~ mp, data = subset(agg, year == 2014), 
+       pch = pch, col = "black", bg = scales::alpha("grey25", 0.25), cex = cex)
+usr = par("usr"); xdiff = diff(usr[1:2]); ydiff = diff(usr[3:4])
+text(usr[2] + xdiff * 0.015, usr[4] - ydiff * 0.125, pos = 2, labels = 2014, font = 2, cex = 1.2)
+legend("bottomright", legend = c("<30", 60, 100, 200, 300, 400), title = "# Smolt",
+       pt.cex = c(1.25, c(60, 100, 200, 300, 400) * 0.01), pch = c(22, 21, 21, 21, 21, 21), 
+       col = "black", pt.bg = scales::alpha("grey25", 0.25), bty = "n")
+
+# 2015 plot
+plot(p_minijack ~ mp, data = subset(agg, year == 2015), las = 1, type = "n", xlim = range(dat$progeny_wt) + c(-5,5), ylim = c(0,1))
+probs = get_probs(fit_null_15, "contrast", F)
+probs = probs[probs$sire_age == 3,]
+polygon(x = c(probs$progeny_wt, rev(probs$progeny_wt)),
+        y = c(probs$lwr95ci, rev(probs$upr95ci)), col = scales::alpha("grey25", 0.25), border = NA)
+lines(lwr95ci ~ progeny_wt, data = probs, col = "grey")
+lines(upr95ci ~ progeny_wt, data = probs, col = "grey")
+lines(prob ~ progeny_wt, data = probs, col = "black", lwd = 2)
+box()
+points(p_minijack ~ mp, data = subset(agg, year == 2015), 
+       pch = pch, col = "black", bg = scales::alpha("grey25", 0.25), cex = cex)
+usr = par("usr"); xdiff = diff(usr[1:2]); ydiff = diff(usr[3:4])
+text(usr[2] + xdiff * 0.015, usr[4] - ydiff * 0.05, pos = 2, labels = 2015, font = 2, cex = 1.2)
+
+# 2016 plot
+plot(p_minijack ~ mp, data = subset(agg, year == 2016), las = 1, type = "n", xlim = range(dat$progeny_wt) + c(-5,5), ylim = c(0,1))
+probs = get_probs(fit_null_16, "contrast", F)
+probs = probs[probs$sire_age == 3,]
+polygon(x = c(probs$progeny_wt, rev(probs$progeny_wt)),
+        y = c(probs$lwr95ci, rev(probs$upr95ci)), col = scales::alpha("grey25", 0.25), border = NA)
+lines(lwr95ci ~ progeny_wt, data = probs, col = "grey")
+lines(upr95ci ~ progeny_wt, data = probs, col = "grey")
+lines(prob ~ progeny_wt, data = probs, col = "black", lwd = 2)
+box()
+points(p_minijack ~ mp, data = subset(agg, year == 2016), 
+       pch = pch, col = "black", bg = scales::alpha("grey25", 0.25), cex = cex)
+mtext(side = 1, outer = T, line = 0.75, "Progeny Weight (g)")
+mtext(side = 2, outer = T, line = 1.5, "Minijack Rate")
+usr = par("usr"); xdiff = diff(usr[1:2]); ydiff = diff(usr[3:4])
+text(usr[2] + xdiff * 0.015, usr[4] - ydiff * 0.05, pos = 2, labels = 2016, font = 2, cex = 1.2)
+dev.off()
+
